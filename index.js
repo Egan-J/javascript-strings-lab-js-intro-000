ad191ef6c17aefe1a1a6d21379a6811121f04cd7
Jessica Egan <jlegan3@gmail.com>
2020-03-30 00:21:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ var topic = "space";
 var conversation = `${topic}`;
 var greeting = "Hello, everybody!"
 var topic = "Let's talk about space.."
-var greetSpecialGuest = "Hello," + specialGuest + "!"
+var greetSpecialGuest = "Hello, " + specialGuest + "!"
